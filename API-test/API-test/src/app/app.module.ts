665fdb00896dfe938520b682dbf40f0f35d89cee
Roy van de Pol <royvdpol@gmail.com>
2022-02-21 22:05:15 +0100

    Made album page fancier

--- API-test/API-test/src/app/app.module.ts
+++ API-test/API-test/src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumsComponent } from './components/albums/albums.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { PhotosComponent } from './components/photos/photos.component';
+import { AlbumComponent } from './components/album/album.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlbumsComponent,
-    PhotosComponent
+    PhotosComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
--- API-test/API-test/src/app/services/getapidata.service.ts
+++ API-test/API-test/src/app/services/getapidata.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetapidataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getPhotoData(): Observable<Photo[]> {
+    return this.http.get<Photo[]>('https://jsonplaceholder.typicode.com/photos')
+  }
+
+  getPhotosByAlbumId(albumId: number): Observable<Photo[]> {
+    return this.http.get<Photo[]>('https://jsonplaceholder.typicode.com/photos').pipe(map(list => list.filter(photo => photo.albumId === albumId)))
+  }
+
+  getAlbumData(): Observable<Album[]> {
+    return this.http.get<Album[]>('https://jsonplaceholder.typicode.com/albums').pipe(map(list => list.splice(0,5)))
+  }
+}
+
+export interface Photo {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+export interface Album {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 }
